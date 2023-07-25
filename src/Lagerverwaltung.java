65334b56f1ad606a9e52accb0a78fa300f18e09f
Dr. Dillinger <peter.dillinger@srh.de>
2023-07-25 11:34:34 +0200

    Hautprogramm für die Lagerverwaltung erstellt, noch unvollständig, Klasse Lager und Lieferant fehlen noch.

---
+++ src/Lagerverwaltung.java
@@ -0,0 +1,13 @@
+public class Lagerverwaltung {
+    public static void main(String[] args) {
+        Lager lager = new Lager(0);
+        Lieferant lieferant1 = new Lieferant(lager);
+        Lieferant lieferant2 = new Lieferant(lager);
+        lieferant1.start();
+        lieferant2.start();
+        // jetzt wird gearbeitet
+        lieferant1.join();
+        lieferant2.join();
+        System.out.println("Summe: " + lager.getBestand() );
+    }
+}
