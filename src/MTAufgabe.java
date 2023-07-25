b11e7d219d77452e17f1eda5a03330321f58cb17
Dr. Dillinger <peter.dillinger@srh.de>
2023-07-25 13:27:08 +0200

    Summenworker im Einsatz, fehlt noch die Zeitmessung.

--- src/MTAufgabe.java
+++ src/MTAufgabe.java
@@ -1,7 +1,9 @@
 public class MTAufgabe {
+    private static final int ARRAY_SIZE = 10_000_000;
+    private static final int NUM_WORKERS = 5;
 
     public static void main(String[] args) {
-        int[] int_array = new int[ 10_000_000 ];
+        int[] int_array = new int[ ARRAY_SIZE ];
 
         // mit zufälligen Werten befüllen
         for (int i = 0; i < int_array.length; i++) {
@@ -15,5 +17,23 @@ public class MTAufgabe {
         System.out.println("Summenwert des Arrays: " + summe);
         zeit = (System.nanoTime() - zeit) / 1000;
         System.out.println("Berechnet in " + zeit + "µs");
+
+        // Gleiche Aufgabe, jetzt mit mehreren Threads
+        SummenWorker[] worker = new SummenWorker[ NUM_WORKERS ];
+        int start = 0;
+        int elementeProThread = ARRAY_SIZE / NUM_WORKERS;
+        for (int i=0; i<NUM_WORKERS; i++) {
+            worker[i] = new SummenWorker(int_array, start, start+elementeProThread);
+            worker[i].start();
+            start += elementeProThread;
+        }
+
+        summe = 0;
+        for (SummenWorker w : worker) {
+            try { w.join(); }
+            catch (InterruptedException ignored) {}
+            summe += w.getErgebnis();
+        }
+        System.out.println("Summenwert des Arrays: " + summe);
     }
 }
