896cc78009d2a8d4d9a270515c8cacafaede402b
Dr. Dillinger <peter.dillinger@srh.de>
2023-07-25 11:17:45 +0200

    Thread hat eine (private) Statusattribut, das anzeigt, ob er noch aktiv arbeitet.

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,21 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         Worker worker = new Worker();
+        System.out.println("Worker Status: " + worker.isRunning());
         worker.start();
 
         for (int i=0; i<10; i++) {
             System.out.println("main: " + i);
+            System.out.println("Worker Status: " + worker.isRunning());
             Thread.sleep(100);
         }
 
-        worker.join();
+        //worker.join(); möglich, aber passives Warten
+        while ( worker.isRunning() ) { // Polling (aktives Warten)
+            System.out.println("main: ein paar kleine Nebenarbeiten");
+            Thread.sleep(500);
+        }
+        System.out.println("Worker Status: " + worker.isRunning());
         System.out.println("Feddisch");
     }
 }
--- src/Worker.java
+++ src/Worker.java
@@ -1,12 +1,20 @@
 public class Worker extends Thread {
 
+    private boolean running = false;
+
+    public boolean isRunning() {
+        return running;
+    }
+
     @Override
     public void run() {
+        running = true;
         // ist wie main(), nur als extra Thread
         for (int i=0; i<10; i++) {
             System.out.println("run: " + i);
             try { sleep(500); }
             catch (InterruptedException ignored) {}
         }
+        running = false;
     }
 }
